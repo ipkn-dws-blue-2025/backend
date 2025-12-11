df485f3b7ec3669ebb741f5bb42d1fd55ae85feb
DenisStepanidenko <stepanidenko.den@mail.ru>
2025-12-12 01:45:42 +0300

    базовая настройка

---
+++ src/main/java/com/itmo/ipkn/team6/controller/VmMonitoringController.java
@@ -0,0 +1,11 @@
+package com.itmo.ipkn.team6.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/itmo/ipkn/team6")
+public class VmMonitoringController {
+
+
+}
